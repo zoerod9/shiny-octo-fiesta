807c409c1dd09da3442c166bdad482d1038736f2
zoerod9 <zoe.rod9767@gmail.com>
2024-08-15 16:13:38 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default function App(){
+    return (
+     <div className="center">
+        <h1>hello</h1>
+        <h2>lets moke</h2>
+        <MyButton/>
+    </div>
+    )
+}
